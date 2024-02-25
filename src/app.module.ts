2294191159ea183c808452eb4ec9ab1ee65a397e
devjohnpark <devjohnpark@gmail.com>
2024-02-25 18:35:04 +0900

    Connection TypeORM to Poestgresql on container using docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({ 
-  imports: [UsersModule], // 생성한 Modules을 import (nest cli로 자동 생성 가능)
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ // 생성한 Modules을 import (nest cli로 자동 생성 가능)
+    UsersModule, 
+    TypeOrmModule.forRoot({ // Nest.js와 TypeOrm 연동
+      // DB Type
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'qkrwnstj13@$',
+      database: 'postgres',
+      entities: [], // DB Models
+      synchronize: true, // nest.js에서 typeorm으로 생성한 데이터와 DB를 자동 동기화
+    }),
+  ], 
+  controllers: [
+    AppController
+  ],
+  providers: [
+    AppService
+  ],
 })
 export class AppModule {} // 생성한 Module들을 읽어들이는 Root Module
